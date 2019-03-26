67ff33129a976781e1e2a41e6c255eee943181ed
Houston <Houston@dhcp-0-59.adfitech.com>
2019-03-26 12:37:06 -0500

    Finish working and refactored palindrome method

--- lib/hkn_palindrome.rb
+++ lib/hkn_palindrome.rb
@@ -1,6 +1,16 @@
 require "hkn_palindrome/version"
 
-module HknPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  #returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  #returns content for palindrome testing
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
 end
