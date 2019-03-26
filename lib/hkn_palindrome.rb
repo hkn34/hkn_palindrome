b2f7b8a8ca093fd3e833e6d759eefa544b547712
Houston <Houston@dhcp-0-59.adfitech.com>
2019-03-26 13:39:01 -0500

    Update palindrome version to detect integer palindromes

--- lib/hkn_palindrome.rb
+++ lib/hkn_palindrome.rb
@@ -1,16 +1,28 @@
 require "hkn_palindrome/version"
 
-class String
+module HknPalindrome
 
   #returns true for a palindrome, false otherwise.
   def palindrome?
+    if processed_content.empty?
+      false
+    else
     processed_content == processed_content.reverse
+    end
   end
 
   private
 
   #returns content for palindrome testing
   def processed_content
-    self.scan(/[a-z]/i).join.downcase
+    self.to_s.scan(/[a-z]*[\d]*/i).join.downcase
   end
+end
+
+class String
+  include HknPalindrome
+end
+
+class Integer
+  include HknPalindrome
 end
